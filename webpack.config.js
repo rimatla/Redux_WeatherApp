47b686cff54658b029967eb06361d08d30236942
Altamir <alt.coelho@gmail.com>
2018-12-10 12:56:55 -0600

    fix security vulnerability :package:

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,33 @@
 module.exports = {
-  entry: [
-    './src/index.js'
-  ],
+  mode: 'development',
+  entry: ['./src/index.js'],
   output: {
     path: __dirname,
     publicPath: '/',
     filename: 'bundle.js'
   },
   module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
+    rules: [
+      {
+        exclude: /node_modules/,
+
+        use: [
+          {
+            loader: 'babel-loader',
+
+            options: {
+              presets: ['react', 'es2015', 'stage-1']
+            }
+          }
+        ]
       }
-    }]
+    ]
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['*', '.js', '.jsx']
   },
   devServer: {
     historyApiFallback: true,
     contentBase: './'
   }
-};
+}
